2d04e8637c6a6afec7517b46e48dde4d57f08353
Edi Abdul Rahman <eddytech03@gmail.com>
2020-03-02 10:29:03 +0800

    Completed

--- app/controllers/payments.js
+++ app/controllers/payments.js
@@ -1,12 +1,29 @@
 module.exports = app => {
+  const paymentsModel = require('../models/payments')(app)
+
   const paymentsCtrl = {}
 
-  paymentsCtrl.getPayments = (req, res) => {
-    res.json([])
+  paymentsCtrl.getPayments = async (req, res) => {
+    try {
+      const payment = await paymentsModel.findOne({ where: { orderId: req.query.orderId } })
+
+      res.json(payment)
+    } catch (err) {
+      res.status(500).json({ msg: 'There is an error' })
+    }
   }
 
-  paymentsCtrl.newPayment = (req, res) => {
-    res.json({ id: 1, status: true })
+  paymentsCtrl.newPayment = async (req, res) => {
+    try {
+      const payment = await paymentsModel.create({
+        orderId: req.body.orderId,
+        isSuccess: Math.floor(Math.random() * Math.floor(2)) === 1
+      })
+
+      res.json(payment)
+    } catch (err) {
+      res.status(500).json({ msg: 'There is an error' })
+    }
   }
 
   return paymentsCtrl
